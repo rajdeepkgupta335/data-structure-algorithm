bdae6240cc7dcc40d33b833e3776920c6d53c769
SUHAIL KHAN (IN) <SUHAIL.KHAN@homecredit.co.in>
2022-07-21 17:10:18 +0530

    binary search

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,7 @@ public class Main {
         System.out.println("Hello World!");
     }
 
-
     public static int addTwoNums(int a, int b){
         return a+b;
-    }
-
-    public static void hello(){
-        System.out.println("hello");
-    }
-    public static void hello(){
-        System.out.println("hello2");
-    }
-    public static void hello(){
-        System.out.println("hello3");
     }
 }
--- src/array/Array.java
+++ src/array/Array.java
@@ -3,17 +3,6 @@ import java.util.*;
 
 public class Array {
 
-    public static int linearSearch(int arr[],int x){
-
-        for (int i=0;i<arr.length;i++){
-            if(arr[i]==x){
-                return i;
-            }
-        }
-
-       return -1;
-    }
-
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
@@ -26,11 +15,36 @@ public class Array {
 
         int x=sc.nextInt();
 
-        System.out.println(linearSearch(arr,x));
-
-
-
+        System.out.println(binarySearch(arr,x));
 
     }
+    public static int linearSearch(int arr[],int x){
 
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public  static int binarySearch (int [] arr, int n){
+        int start = 0, end =arr.length-1;
+
+        while(start<=end){
+            int mid = start - (start -end)/2;
+            if(arr[mid]==n){
+                return mid;
+            }else if(arr[mid]>n){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+
+        }
+
+        return -1;
+    }
 }
